c19b641239103f13f57e1cb8d72ab99a842bc512
o-shea <atmos.o.shea@gmail.com>
2023-08-09 01:16:56 +0400

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by MAC on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
